eb2d5f78d3ab81ea5851c820caebe94f46f54155
ivan_714 <l1ft@yandex.ru>
2021-04-11 12:20:29 +0400

    Added autosaving/

--- main.py
+++ main.py
@@ -16,7 +16,22 @@ def my_round(var, size):
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     global CODE_FTXFutures
+    global LEVERAGE_FTXFutures
+    global DEPO_FTXFutures
+    global PART1_FTXFutures
+    global PART2_FTXFutures
+    global PART3_FTXFutures
+    global PART4_FTXFutures
+    global PART5_FTXFutures
+
     global CODE_BinanceFutures
+    global LEVERAGE_BinanceFutures
+    global DEPO_BinanceFutures
+    global PART1_BinanceFutures
+    global PART2_BinanceFutures
+    global PART3_BinanceFutures
+    global PART4_BinanceFutures
+    global PART5_BinanceFutures
 
     def __init__(self):
 
@@ -37,15 +52,24 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         if os.path.exists('config.ini'):
             with open('config.ini', "r") as f:
                 data = f.read()
-                self.CODE_FTXFutures = data.split('\n', 10)[0]
-                self.CODE_BinanceFutures = data.split('\n', 10)[1]
-                self.editLeverage.setText(data.split('\n', 10)[2])
-                self.editPart1.setText(data.split('\n', 10)[3])
-                self.editPart2.setText(data.split('\n', 10)[4])
-                self.editPart3.setText(data.split('\n', 10)[5])
-                self.editPart4.setText(data.split('\n', 10)[6])
-                self.editPart5.setText(data.split('\n', 10)[7])
-                self.editDepo.setText(data.split('\n', 10)[8])
+                self.CODE_FTXFutures = data.split('\n', 20)[0]
+                self.LEVERAGE_FTXFutures = data.split('\n', 20)[1]
+                self.DEPO_FTXFutures = data.split('\n', 20)[2]
+                self.PART1_FTXFutures = data.split('\n', 20)[3]
+                self.PART2_FTXFutures = data.split('\n', 20)[4]
+                self.PART3_FTXFutures = data.split('\n', 20)[5]
+                self.PART4_FTXFutures = data.split('\n', 20)[6]
+                self.PART5_FTXFutures = data.split('\n', 20)[7]
+
+                self.CODE_BinanceFutures = data.split('\n', 20)[8]
+                self.LEVERAGE_BinanceFutures = data.split('\n', 20)[9]
+                self.DEPO_BinanceFutures = data.split('\n', 20)[10]
+                self.PART1_BinanceFutures = data.split('\n', 20)[11]
+                self.PART2_BinanceFutures = data.split('\n', 20)[12]
+                self.PART3_BinanceFutures = data.split('\n', 20)[13]
+                self.PART4_BinanceFutures = data.split('\n', 20)[14]
+                self.PART5_BinanceFutures = data.split('\n', 20)[15]
+
         else:
             self.listWidget.addItem('Ошибка! Файл config.ini не найден')
             self.CODE_FTXFutures = ''
@@ -58,15 +82,73 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             self.editPart5.setText('0')
 
         self.editCode.setText(self.CODE_FTXFutures)
-        print(self.CODE_BinanceFutures)
-        self.comboBox.currentIndexChanged.connect(self.comboBoxChanged)
-        self.editCode.textChanged.connect(self.codeChanged)
+        self.editLeverage.setText(self.LEVERAGE_FTXFutures)
+        self.editPart1.setText(self.PART1_FTXFutures)
+        self.editPart2.setText(self.PART2_FTXFutures)
+        self.editPart3.setText(self.PART3_FTXFutures)
+        self.editPart4.setText(self.PART4_FTXFutures)
+        self.editPart5.setText(self.PART5_FTXFutures)
+        self.editDepo.setText(self.DEPO_FTXFutures)
 
-    def codeChanged(self):
+        self.comboBox.currentIndexChanged.connect(self.comboBoxChanged)
+
+        # CONNECTIONS
+        self.editCode.textChanged.connect(self.editCodeChanged)
+        self.editLeverage.textChanged.connect(self.editLeverageChanged)
+        self.editPart1.textChanged.connect(self.editPart1Changed)
+        self.editPart2.textChanged.connect(self.editPart2Changed)
+        self.editPart3.textChanged.connect(self.editPart3Changed)
+        self.editPart4.textChanged.connect(self.editPart4Changed)
+        self.editPart5.textChanged.connect(self.editPart5Changed)
+        self.editDepo.textChanged.connect(self.editDepoChanged)
+
+    def editCodeChanged(self):
         if self.comboBox.currentIndex() == 0:
             self.CODE_FTXFutures = self.editCode.text()
         elif self.comboBox.currentIndex() == 2:
             self.CODE_BinanceFutures = self.editCode.text()
+
+    def editLeverageChanged(self):
+        if self.comboBox.currentIndex() == 0:
+            self.LEVERAGE_FTXFutures = self.editLeverage.text()
+        elif self.comboBox.currentIndex() == 2:
+            self.LEVERAGE_BinanceFutures = self.editLeverage.text()
+
+    def editDepoChanged(self):
+        if self.comboBox.currentIndex() == 0:
+            self.DEPO_FTXFutures = self.editDepo.text()
+        elif self.comboBox.currentIndex() == 2:
+            self.DEPO_BinanceFutures = self.editDepo.text()
+
+    def editPart1Changed(self):
+        if self.comboBox.currentIndex() == 0:
+            self.PART1_FTXFutures = self.editPart1.text()
+        elif self.comboBox.currentIndex() == 2:
+            self.PART1_BinanceFutures = self.editPart1.text()
+
+    def editPart2Changed(self):
+        if self.comboBox.currentIndex() == 0:
+            self.PART2_FTXFutures = self.editPart2.text()
+        elif self.comboBox.currentIndex() == 2:
+            self.PART2_BinanceFutures = self.editPart2.text()
+
+    def editPart3Changed(self):
+        if self.comboBox.currentIndex() == 0:
+            self.PART3_FTXFutures = self.editPart3.text()
+        elif self.comboBox.currentIndex() == 2:
+            self.PART3_BinanceFutures = self.editPart3.text()
+
+    def editPart4Changed(self):
+        if self.comboBox.currentIndex() == 0:
+            self.PART4_FTXFutures = self.editPart4.text()
+        elif self.comboBox.currentIndex() == 2:
+            self.PART4_BinanceFutures = self.editPart4.text()
+
+    def editPart5Changed(self):
+        if self.comboBox.currentIndex() == 0:
+            self.PART5_FTXFutures = self.editPart5.text()
+        elif self.comboBox.currentIndex() == 2:
+            self.PART5_BinanceFutures = self.editPart5.text()
 
     def comboBoxChanged(self):
         self.listWidget.clear()
@@ -80,9 +162,23 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
         if self.comboBox.currentIndex() == 0:
             self.editCode.setText(self.CODE_FTXFutures)
+            self.editLeverage.setText(self.LEVERAGE_FTXFutures)
+            self.editPart1.setText(self.PART1_FTXFutures)
+            self.editPart2.setText(self.PART2_FTXFutures)
+            self.editPart3.setText(self.PART3_FTXFutures)
+            self.editPart4.setText(self.PART4_FTXFutures)
+            self.editPart5.setText(self.PART5_FTXFutures)
+            self.editDepo.setText(self.DEPO_FTXFutures)
 
-        if self.comboBox.currentIndex() == 2:
+        elif self.comboBox.currentIndex() == 2:
             self.editCode.setText(self.CODE_BinanceFutures)
+            self.editLeverage.setText(self.LEVERAGE_BinanceFutures)
+            self.editPart1.setText(self.PART1_BinanceFutures)
+            self.editPart2.setText(self.PART2_BinanceFutures)
+            self.editPart3.setText(self.PART3_BinanceFutures)
+            self.editPart4.setText(self.PART4_BinanceFutures)
+            self.editPart5.setText(self.PART5_BinanceFutures)
+            self.editDepo.setText(self.DEPO_BinanceFutures)
 
         if (self.comboBox.currentIndex() == 1) or (self.comboBox.currentIndex() == 3):
             self.editLeverage.setEnabled(False)
@@ -92,18 +188,31 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def closeEvent(self, event):
         with open('config.ini', "w") as f:
             f.write(self.CODE_FTXFutures + '\n')
+            f.write(self.LEVERAGE_FTXFutures + '\n')
+            f.write(self.DEPO_FTXFutures + '\n')
+            f.write(self.PART1_FTXFutures + '\n')
+            f.write(self.PART2_FTXFutures + '\n')
+            f.write(self.PART3_FTXFutures + '\n')
+            f.write(self.PART4_FTXFutures + '\n')
+            f.write(self.PART5_FTXFutures + '\n')
+
             f.write(self.CODE_BinanceFutures + '\n')
-            f.write(self.editLeverage.text() + '\n')
-            f.write(self.editPart1.text() + '\n')
-            f.write(self.editPart2.text() + '\n')
-            f.write(self.editPart3.text() + '\n')
-            f.write(self.editPart4.text() + '\n')
-            f.write(self.editPart5.text() + '\n')
-            f.write(self.editDepo.text())
+            f.write(self.LEVERAGE_BinanceFutures + '\n')
+            f.write(self.DEPO_BinanceFutures + '\n')
+            f.write(self.PART1_BinanceFutures + '\n')
+            f.write(self.PART2_BinanceFutures + '\n')
+            f.write(self.PART3_BinanceFutures + '\n')
+            f.write(self.PART4_BinanceFutures + '\n')
+            f.write(self.PART5_BinanceFutures + '\n')
 
     def writeToFile(self, MVS_DIR, ACCOUNT_CODE, ex_prefix, ticker, depo, price, size, PART1, PART2, PART3, PART4, PART5):
         volume_max = float(depo) / float(price)
         size = float(size)
+        PART1 = float(PART1)
+        PART2 = float(PART2)
+        PART3 = float(PART3)
+        PART4 = float(PART4)
+        PART5 = float(PART5)
         Vol1 = my_round(volume_max * PART1 / 100, size)
         Vol2 = my_round(volume_max * PART2 / 100, size)
         Vol3 = my_round(volume_max * PART3 / 100, size)
@@ -274,7 +383,7 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             ex_prefix = 'FTXD.FUT.'
             api_endpoint = "https://ftx.com/api/futures"
             json_data = requests.get(api_endpoint).json()
-            depo = depo * LEVERAGE
+            depo = float(self.DEPO_FTXFutures) * float(self.LEVERAGE_FTXFutures)
 
             for item in json_data['result']:
                 if (item['expiryDescription'] == 'Perpetual'):
@@ -286,7 +395,7 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                     PriceAggregationStep = 10
                     punkti = math.ceil((price * 0.0007) / (PriceAggregationStep * priceIncrement));
 
-                    if self.writeToFile(MVS_DIR, self.CODE_FTXFutures, ex_prefix, ticker, depo, price, size, PART1, PART2, PART3, PART4, PART5):
+                    if self.writeToFile(MVS_DIR, self.CODE_FTXFutures, ex_prefix, ticker, depo, price, size, self.PART1_FTXFutures, self.PART2_FTXFutures, self.PART3_FTXFutures, self.PART4_FTXFutures, self.PART5_FTXFutures):
                         number_updated_files += 1
                     else:
                         number_skipped_tickers += 1
@@ -298,8 +407,7 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             api_endpoint_premiumIndex = "https://binance.com/fapi/v1/premiumIndex"
             json_data_exchange = requests.get(api_endpoint_exchange).json()
             json_data_premiumIndex = requests.get(api_endpoint_premiumIndex).json()
-            depo = depo * LEVERAGE
-            ACCOUNT_CODE = self.editCode.text()
+            depo = float(self.DEPO_BinanceFutures) * float(self.LEVERAGE_BinanceFutures)
 
             for item in json_data_exchange['symbols']:
                 ticker = item['symbol']
@@ -310,7 +418,7 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                     if item['symbol'] == ticker:
                         price = item['markPrice']
 
-                        if self.writeToFile(MVS_DIR, self.CODE_BinanceFutures, ex_prefix, ticker, depo, price, size, PART1, PART2, PART3, PART4, PART5):
+                        if self.writeToFile(MVS_DIR, self.CODE_BinanceFutures, ex_prefix, ticker, depo, price, size, self.PART1_BinanceFutures, self.PART2_BinanceFutures, self.PART3_BinanceFutures, self.PART4_BinanceFutures, self.PART5_BinanceFutures):
                             number_updated_files += 1
                         else:
                             number_skipped_tickers += 1
